ba1a4978dd9be4d2a4a29db5622fd09cafb4797a
Alexis Torres <alexistorres305@gmail.com>
2023-01-22 15:25:08 -0800

    Initialize pw/59/updateDb

---
+++ app/utils.ts
@@ -0,0 +1,39 @@
+import { useMemo } from "react";
+import { useMatches } from "@remix-run/react";
+import type { User } from "./models/user.server";
+
+export function useMatchesData(id: string) {
+  const matchingRoutes = useMatches();
+  const route = useMemo(
+    () => matchingRoutes.find((route) => route.id === id),
+    [matchingRoutes, id]
+  );
+
+  return route?.data;
+}
+
+export function isUser(user: User) {
+  return user && typeof user === "object";
+}
+
+export function useOptionalUser() {
+  const data = useMatchesData("root");
+  if (!data || !isUser(data.user)) {
+    return undefined;
+  }
+  return data.user;
+}
+
+export function useUser() {
+  const maybeUser = useOptionalUser();
+  if (!maybeUser) {
+    throw new Error(
+      "No user found in root loader, but user is required by useUser. If user is optional, try useOptionalUser instead."
+    );
+  }
+  return maybeUser;
+}
+
+export function validateEmail(email: unknown): email is string {
+  return typeof email === "string" && email.length > 3 && email.includes("@");
+}
