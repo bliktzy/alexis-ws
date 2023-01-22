03fddcdd1c4a71cd523e577dfaefbd70b0ba4b2e
Alexis Torres <alexistorres305@gmail.com>
2023-01-22 15:25:10 -0800

    Initialize pw/investigateESBuild

---
+++ app/routes/notes/$noteId.tsx
@@ -0,0 +1,52 @@
+import type { ActionFunction, LoaderArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import type { Note } from "~/models/note.server";
+import { deleteNote, getNote } from "~/models/note.server";
+import { requireUserId } from "~/session.server";
+import invariant from "tiny-invariant";
+
+type LoaderData = {
+  note: Note;
+};
+
+export async function loader({ request, params }: LoaderArgs) {
+  const userId = await requireUserId(request);
+  invariant(params.noteId, "noteId not found");
+
+  const note = await getNote({ userId, id: params.noteId });
+  if (!note) {
+    throw new Response("Not Found", { status: 404 });
+  }
+
+  return json({ note });
+};
+
+export const action: ActionFunction = async ({ request, params }) => {
+  const userId = await requireUserId(request);
+  invariant(params.noteId, "noteId not found");
+
+  await deleteNote({ userId, id: params.noteId });
+
+  return redirect("/notes");
+};
+
+export default function NoteDetailsPage() {
+  const data = useLoaderData<typeof loader>() as LoaderData;
+
+  return (
+    <div>
+      <h3 className="text-2xl font-bold">{data.note.title}</h3>
+      <p className="py-6">{data.note.body}</p>
+      <hr className="my-4" />
+      <Form method="post">
+        <button
+          type="submit"
+          className="rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+        >
+          Delete
+        </button>
+      </Form>
+    </div>
+  );
+}
