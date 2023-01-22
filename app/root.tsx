fb982ecef22ff2a603c885a084f2e700a0516186
Alexis Torres <alexistorres305@gmail.com>
2023-01-22 15:25:09 -0800

    Initialize tzm/addCypressVidsToGitignore

---
+++ app/root.tsx
@@ -0,0 +1,50 @@
+import type {
+  LinksFunction,
+  LoaderFunction,
+  MetaFunction,
+} from "@remix-run/node";
+import { json } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import tailwindStylesheetUrl from "./styles/tailwind.css";
+import { getUser } from "./session.server";
+
+export const meta: MetaFunction = () => {
+  return { title: "New Remix App" };
+};
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  return json({
+    user: await getUser(request),
+  });
+};
+
+export default function App() {
+  return (
+    <html lang="en" className="h-full">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="h-full">
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
